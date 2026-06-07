4199367a729401d3078834e8570402868c6057fb
Natasha Libera <tash@artoftash.com>
2026-06-07 17:48:45 +0200

    Added checks and 400 errors to forms.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from models import db, Movie
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, abort
 from data_manager import DataManager
 from movie_lookup import get_movie_details
 
@@ -19,6 +19,27 @@ db.init_app(app)
 dm = DataManager()
 
 
+# Param helpers
+
+class MissingParamError(Exception):
+    pass
+
+
+def extract_required(dict, key):
+    """ Throws an error if dict[key] is empty or None. """
+    value = dict.get(key, None)
+    if value is None or value == '':
+        raise MissingParamError()
+    return value
+
+def extract_optional(dict, key):
+    """ Allows dict[key] to be None, and returns empty string as None. """
+    value = dict.get(key, None)
+    if value == '':
+        value = None
+    return value
+
+
 # Routes
 
 @app.route('/')
@@ -34,9 +55,12 @@ def list_users():
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    name = request.form['name']
-    dm.create_user(name)
-    return redirect(url_for('home'))
+    try:
+        name = extract_required(request.form, 'name')
+        dm.create_user(name)
+        return redirect(url_for('home'))
+    except MissingParamError:
+        abort(400)
 
 
 @app.route('/users/<int:user_id>/movies', methods=['GET'])
@@ -49,28 +73,31 @@ def get_movies(user_id):
 @app.route('/users/<int:user_id>/movies', methods=['POST'])
 def add_movie(user_id):
     user = dm.get_user(user_id)
-    title = request.form['title']
-    year = request.form['year']
-    if year == '':
-        year = None
-    director = None
-    poster_url = None
-    if year:
-        year = int(year)
-    movie_details = get_movie_details(title, year)
-    if movie_details is None:
-        pass  # Leave details as specified by the User.
-    else:
-        title, year, director, poster_url = movie_details
-    new_movie = Movie(
-        user=user,
-        title=title,
-        year=year,
-        director=director,
-        poster_url = poster_url,
-    )
-    dm.add_movie(new_movie)
-    return redirect(url_for('get_movies', user_id=user_id))
+    try:
+        title = extract_required(request.form, 'title')
+        year = extract_optional(request.form, 'year')
+        director = None
+        poster_url = None
+        if year:
+            year = int(year)
+        movie_details = get_movie_details(title, year)
+        if movie_details is None:
+            pass  # Leave details as specified by the User.
+        else:
+            title, year, director, poster_url = movie_details
+        new_movie = Movie(
+            user=user,
+            title=title,
+            year=year,
+            director=director,
+            poster_url = poster_url,
+        )
+        dm.add_movie(new_movie)
+        return redirect(url_for('get_movies', user_id=user_id))
+    except MissingParamError:
+        abort(400)
+    except ValueError:
+        abort(400)
 
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/update',
@@ -78,23 +105,28 @@ def add_movie(user_id):
 def update_movie(user_id, movie_id):
     user = dm.get_user(user_id)
     movie = dm.get_movie(movie_id)
-    title = request.form['title']
-    year = movie.year
-    if year:
-        year = int(year)
-    movie_details = get_movie_details(title, year)
-    if movie_details is None:
-        # Leave details as specified by the User.
-        movie.title = title
-        dm.update_movie(movie)
-    else:
-        title, year, director, poster_url = movie_details
-        movie.title = title
-        movie.year = year
-        movie.director = director
-        movie.poster_url = poster_url
-        dm.update_movie(movie)
-    return redirect(url_for('get_movies', user_id=user_id))
+    try:
+        title = extract_required(request.form, 'title')
+        year = movie.year
+        if year:
+            year = int(year)
+        movie_details = get_movie_details(title, year)
+        if movie_details is None:
+            # Leave details as specified by the User.
+            movie.title = title
+            dm.update_movie(movie)
+        else:
+            title, year, director, poster_url = movie_details
+            movie.title = title
+            movie.year = year
+            movie.director = director
+            movie.poster_url = poster_url
+            dm.update_movie(movie)
+        return redirect(url_for('get_movies', user_id=user_id))
+    except MissingParamError:
+        abort(400)
+    except ValueError:
+        abort(400)
 
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete',
@@ -104,7 +136,7 @@ def delete_movie(user_id, movie_id):
     return redirect(url_for('get_movies', user_id=user_id))
 
 
-# Define Custom Error Handling
+# Custom Error Pages
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -113,14 +145,14 @@ def page_not_found(e):
         message="This page does not exist.",
     ), 404
 
-
 @app.errorhandler(400)
 def bad_request(e):
     return render_template('error.html',
         code=400,
-        message="The expected inputs were not received by the server. This may happen if you to try to submit a form or a request in a way that wasn't intended by the developers."
+        message=("Bad request. "
+                 "This may happen if you to try to submit a form in a way "
+                 "that wasn't intended by the developers.")
     ), 400
-
 
 @app.errorhandler(500)
 def server_error(e):
