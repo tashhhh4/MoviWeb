59696aad1d525086d11e7ce9df099feee20d6de3
Natasha Libera <tash@artoftash.com>
2026-06-07 18:17:09 +0200

    Removed need to manually configure a database file

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ pip install -r requirements.txt
 
 Add values for the following variables in a file called 'secret.py':
 API_KEY=<api key from OMDB API>
-DB_FILE=<pathname to sqlite datafile>
 
 ## How to Run
 ```
--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    f'sqlite:///{os.path.join(basedir, secret.DB_FILE)}')
+    f'sqlite:///{os.path.join(basedir, "data.sqlite")}')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
